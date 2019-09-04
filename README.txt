28064012c72ef08d6a0ab4b222dba6f14e5e70cc
davidreoch <dreoch1@gmail.com>
2019-09-04 14:56:29 +0100

    final working commented code

---
+++ README.txt
@@ -0,0 +1,8 @@
+Twig Coding Challenge
+
+How to run:
+
+- Firstly, clone the repo!
+- Run from the command line using node specifying the file to run followed by the input
+- e.g. node twig.js 1 2 3 4 5 3
+- result will be printed to the console
--- twig.js
+++ twig.js
@@ -1,12 +1,27 @@
+// Take in args from command line in the form node [filename] val1 val2 val3 val4 chunkSize
 var myArgs = process.argv.slice(2);
-console.log('myArgs: ', myArgs);
+const arraySize = parseInt(myArgs.length - 1);
+const chunkSize = parseInt(myArgs.pop());
 
-const arraySize = myArgs.length-1;
-const splitSize = myArgs[myArgs.length-1];
+// the amount to divide by, round number up or down to nearest whole number
+const divideAmount = Math.round(arraySize / chunkSize);
 
-var output = [];
+console.log(arraySize / chunkSize);
 
-let x = arraySize % splitSize;
-console.log('remainder = ' + x);
+console.log(divideAmount);
 
+// assign output to returned value
+const output = chunk(myArgs, divideAmount);
 
+// function which cretes array of elements split equally where possible
+function chunk(arr, divideAmount) {
+    var ret = [];
+    // loop through array incrementing i by division amount
+    for (var i = 0; i < arraySize; i += divideAmount) {
+        // push each subarray to ret array
+        ret.push(arr.slice(i, i + divideAmount));
+    }
+    return ret;
+}
+
+console.log(output);
